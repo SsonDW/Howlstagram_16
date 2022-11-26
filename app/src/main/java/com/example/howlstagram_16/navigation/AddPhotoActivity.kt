ec59eff34c8b86b35a550cf1bc4a042d899d31d9
dongu <1141501@naver.com>
2022-11-26 22:00:04 +0900

    layitme_detai.xml

--- app/src/main/java/com/example/howlstagram_16/navigation/AddPhotoActivity.kt
+++ app/src/main/java/com/example/howlstagram_16/navigation/AddPhotoActivity.kt
@@ -1,43 +1,48 @@
 package com.example.howlstagram_16.navigation
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
-import android.icu.text.SimpleDateFormat
 import android.net.Uri
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import com.example.howlstagram_16.R
+import com.example.howlstagram_16.navigation.model.ContentDTO
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.UploadTask
 import kotlinx.android.synthetic.main.activity_add_photo.*
+import java.text.SimpleDateFormat
 import java.util.*
 
+@Suppress("DEPRECATION")
 class AddPhotoActivity : AppCompatActivity() {
-    var PICK_IMAGE_FROM_ALBUM = 0
+    var PICK_IMAGE_FROM_ALBUM =0
     var storage : FirebaseStorage? = null
     var photoUri : Uri? = null
-//    var auth : FirebaseAuth? = null
-//    var firestore : FirebaseFirestore? = null
-    @RequiresApi(Build.VERSION_CODES.N)
+    var auth : FirebaseAuth? = null
+    var firestore : FirebaseFirestore? = null
+
+//    private lateinit var binding : ActivityAddPhotoBinding
+//    private lateinit var getResult: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        binding = ActivityAddPhotoBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
         setContentView(R.layout.activity_add_photo)
 
         //Initiate
         storage = FirebaseStorage.getInstance()
-//      auth = FirebaseAuth.getInstance()
-//      firestore = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
 
         //Open the album
         var photoPickerIntent = Intent(Intent.ACTION_PICK)
         photoPickerIntent.type = "image/*"
-        startActivityForResult(photoPickerIntent, PICK_IMAGE_FROM_ALBUM)
+        startActivityForResult(photoPickerIntent,PICK_IMAGE_FROM_ALBUM)
 
         //add image upload event
         addphoto_btn_upload.setOnClickListener {
@@ -45,33 +50,80 @@ class AddPhotoActivity : AppCompatActivity() {
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.N)
-    private fun contentUpload() {
-        //Make filename
-
-        var timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        var imageFileName = "IMAGE_" + timestamp + "_.png"
-
-        var storageRef = storage?.reference?.child("images")?.child(imageFileName)
-
-        //Promise method
-        storageRef?.putFile(photoUri!!)?.addOnSuccessListener {
-            Toast.makeText(this,getString(R.string.upload_success), Toast.LENGTH_LONG).show()
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data:Intent?){
+        super.onActivityResult(requestCode, resultCode,data)
         if(requestCode == PICK_IMAGE_FROM_ALBUM){
             if(resultCode == Activity.RESULT_OK){
-                //This is path to the selected image
                 photoUri = data?.data
                 addphoto_image.setImageURI(photoUri)
-
-            }else{
-                //Exit the addPhotoActivity if you leave the album without selecting it
+            }
+            else{
                 finish()
             }
         }
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    fun contentUpload(){
+        //Make filename
+        val timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageFileName= "IMAGE_" + timestamp+"_.png"
+        val storageRef=storage?.reference?.child("images")?.child(imageFileName)
+        //Promise method
+        storageRef?.putFile(photoUri!!)?.continueWithTask{ task: Task<UploadTask.TaskSnapshot> ->
+            return@continueWithTask storageRef.downloadUrl
+        }?.addOnSuccessListener { uri->
+            var contentDTO = ContentDTO()
+
+            //Insert downloadUri of image
+            contentDTO.imageUrl = uri.toString()
+
+            //Insert uid of user
+            contentDTO.uid = auth?.currentUser?.uid
+
+            //Insert userId
+            contentDTO.userId = auth?.currentUser?.email
+
+            //Insert explain of content
+            contentDTO.explain = addphoto_edit_explain.text.toString()
+
+            //Insert timestamp
+            contentDTO.timestamp = System.currentTimeMillis()
+
+            firestore?.collection("images")?.document()?.set(contentDTO)
+
+            setResult(Activity.RESULT_OK)
+
+            finish()
+        }
+
+        //Callback method
+//        storageRef?.putFile(photoUri!!)?.addOnSuccessListener{
+//            storageRef.downloadUrl.addOnSuccessListener { uri ->
+//                var contentDTO = ContentDTO()
+//
+//                //Insert downloadUri of image
+//                contentDTO.imageUrl = uri.toString()
+//
+//                //Insert uid of user
+//                contentDTO.uid = auth?.currentUser?.uid
+//
+//                //Insert userId
+//                contentDTO.userId = auth?.currentUser?.email
+//
+//                //Insert explain of content
+//                contentDTO.explain = addphoto_edit_explain.text.toString()
+//
+//                //Insert timestamp
+//                contentDTO.timestamp = System.currentTimeMillis()
+//
+//                firestore?.collection("images")?.document()?.set(contentDTO)
+//
+//                setResult(Activity.RESULT_OK)
+//
+//                finish()
+//            }
+//        }
     }
 }
