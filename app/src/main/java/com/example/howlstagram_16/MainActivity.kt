1d825ef1bc95cf9e557c25d34dba29c5dd73bdec
dongu <1141501@naver.com>
2022-11-26 12:16:06 +0900

    addphoto_image

--- app/src/main/java/com/example/howlstagram_16/MainActivity.kt
+++ app/src/main/java/com/example/howlstagram_16/MainActivity.kt
@@ -1,45 +1,54 @@
 package com.example.howlstagram_16
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.howlstagram_16.navigation.*
 import com.google.android.material.navigation.NavigationBarView
 import kotlinx.android.synthetic.main.activity_main.*
-import navigation.AlarmFragment
-import navigation.DetailViewFragment
-import navigation.GridFragment
-import navigation.UserFragment
+//import navigation.AlarmFragment
+//import navigation.DetailViewFragment
+//import navigation.GridFragment
+//import navigation.UserFragment
 
 class MainActivity : AppCompatActivity() , NavigationBarView.OnItemSelectedListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottom_navigation.setOnItemSelectedListener(this)
+        ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
        when(item.itemId){
            R.id.action_home->{
-               var detailViewFragment=DetailViewFragment()
+               var detailViewFragment= DetailViewFragment()
                supportFragmentManager.beginTransaction().replace(R.id.main_content,detailViewFragment).commit()
                return true
            }
            R.id.action_search->{
-               var gridFragment=GridFragment()
+               var gridFragment= GridFragment()
                supportFragmentManager.beginTransaction().replace(R.id.main_content,gridFragment).commit()
                return true
            }
            R.id.action_add_photo->{
-
+                if(ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    startActivity(Intent(this, AddPhotoActivity::class.java))
+                }
                return true
            }
            R.id.action_favorite_alarm->{
-               var alarmFragment=AlarmFragment()
+               var alarmFragment= AlarmFragment()
                supportFragmentManager.beginTransaction().replace(R.id.main_content,alarmFragment).commit()
                return true
            }
            R.id.action_account->{
-               var userFragment=UserFragment()
+               var userFragment= UserFragment()
                supportFragmentManager.beginTransaction().replace(R.id.main_content,userFragment).commit()
                return true
            }
