efc9c479d4dade5b28004b0757b57d54f4321761
doeun Kim <doeun0509@naver.com>
2022-11-27 00:08:27 +0900

    사진 업로드

--- app/src/main/java/com/example/howlstagram_16/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/example/howlstagram_16/navigation/DetailViewFragment.kt
@@ -11,16 +11,20 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.howlstagram_16.R
 import com.example.howlstagram_16.navigation.model.ContentDTO
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_detail.view.*
 import kotlinx.android.synthetic.main.item_detail.view.*
 
 class DetailViewFragment : Fragment() {
+    var uid:String?=null
     var firestore : FirebaseFirestore? = null
     override fun onCreateView(inflater: LayoutInflater, container:  ViewGroup?, savedInstanceState: Bundle?): View?{
 
         val view= LayoutInflater.from(activity).inflate(R.layout.fragment_detail, container, false)
         firestore = FirebaseFirestore.getInstance()
+        uid=FirebaseAuth.getInstance().currentUser?.uid
+
         view.detailviewfragment_recyclerview.adapter = DetailViewRecyclerViewAdapter()
         view.detailviewfragment_recyclerview.layoutManager=LinearLayoutManager(activity)
         return view
@@ -60,13 +64,51 @@ class DetailViewFragment : Fragment() {
         override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
             val viewholder= (p0 as CustomViewHolder).itemView
 
+            //userId
             viewholder.detailviewitem_profile_textview.text = contentDTOs!![p1].userId
+            //Image
             Glide.with(p0.itemView.context).load(contentDTOs!![p1].imageUrl).into(viewholder.detailviewitem_imageview_content)
+            //Explain of content
             viewholder.detailviewitem_explain_textview.text=contentDTOs!![p1].explain
+            //likes
             viewholder.detailviewitem_favoritecounter_textview.text="Likes"+contentDTOs!![p1].favoriteCount
-            Glide.with(p0.itemView.context).load(contentDTOs!![p1].imageUrl).into(viewholder.detailviewitem_profile_image)
+
+            //좋아요버튼 누를때
+            viewholder.detailviewitem_favorite_imageview.setOnClickListener{
+                favoriteEvent(p1)
+            }
+            if(contentDTOs!![p1].favorites.containsKey(uid)){//uid가 포함되어잇는 경우->좋아요누른경우
+                viewholder.detailviewitem_favorite_imageview.setImageResource(R.drawable.ic_favorite)//좋아요 하트 색칠
+            }
+            else{
+                viewholder.detailviewitem_favorite_imageview.setImageResource(R.drawable.ic_favorite_border)//빈 하트
+            }
         }
 
+        //버튼이 눌리면 실행
+
+
+        fun favoriteEvent(position:Int){
+            var tsDoc =firestore?.collection("images")?.document(contentUidList[position])
+            firestore?.runTransaction { transaction->
+
+                var contentDTO=transaction.get(tsDoc!!).toObject(ContentDTO::class.java)
+
+                if(contentDTO!!.favorites.containsKey(uid)){
+                    //좋아요가 눌려져있을때
+                    //좋아요 취소
+                    contentDTO.favoriteCount = contentDTO.favoriteCount - 1 //좋아요 수 감소
+                    contentDTO.favorites.remove(uid); //uid 제거
+
+                }
+                else{
+                    //좋아요가 안눌러져있을때
+                    contentDTO.favoriteCount = contentDTO.favoriteCount + 1//좋아요 수 증가
+                    contentDTO.favorites[uid!!] = true //uid 추가
+                }
+                transaction.set(tsDoc,contentDTO) //트랜잭션을 서버로 돌려준다
+            }
+        }
 
     }
 }
